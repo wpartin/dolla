dae8fc1e2a1861cd3c74d5f361d0551615f7c331
Weston Partin <djwest91@gmail.com>
2021-06-22 17:52:55 -0500

    added scroll effects

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -33,11 +33,16 @@ const HeroSection = () => {
         </HeroP>
         <HeroBtnWrapper>
           <Button
-            to="/signup"
+            to="signup"
             onMouseEnter={onHover}
             onMouseLeave={onHover}
             primary={"true"}
             dark={"true"}
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
           >
             Get Started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
