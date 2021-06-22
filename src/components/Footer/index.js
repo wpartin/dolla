7d5e782bc5e4866cdb2b21d859d641020b010372
Weston Partin <djwest91@gmail.com>
2021-06-22 16:39:15 -0500

    built footer & elements

---
+++ src/components/Footer/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIconLink,
+  SocialIcons,
+} from "./FooterElements";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact us</FooterLinkTitle>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">Destinations</FooterLink>
+              <FooterLink to="/signin">Sponsorships</FooterLink>
+              <FooterLink to="/signin">Partners</FooterLink>
+              <FooterLink to="/signin">Advertising</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/signin">Submit a Video</FooterLink>
+              <FooterLink to="/signin">Ambassadors</FooterLink>
+              <FooterLink to="/signin">Agencies</FooterLink>
+              <FooterLink to="/signin">Influencers</FooterLink>
+              <FooterLink to="/signin">Feature Breakdowns</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">TikTok</FooterLink>
+              <FooterLink to="/signin">Twitter</FooterLink>
+              <FooterLink to="/signin">YouTube</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">dolla</SocialLogo>
+            <WebsiteRights>
+              dolla &copy; {new Date().getFullYear()} All Rights Reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
