0080fe737820c1775503931307ddef033b0acbe4
Weston Partin <djwest91@gmail.com>
2021-06-22 17:15:57 -0500

    added z-index to active

--- src/components/NavBar/NavbarElements.js
+++ src/components/NavBar/NavbarElements.js
@@ -3,9 +3,9 @@ import { Link as LinkR } from "react-router-dom";
 import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav`
-  background: #000;
+  background: ${({ scrollNav }) => (scrollNav ? "#000" : "transparent")};
   height: 80px;
-  //   margin-top: -80px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -82,6 +82,7 @@ export const NavLinks = styled(LinkS)`
   cursor: pointer;
 
   &.active {
+    z-index: 1000;
     border-bottom: 3px solid #01bf71;
   }
 `;
